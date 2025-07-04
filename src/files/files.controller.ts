d5a4c50a5098f889f6722ee5c7dcd71ca58404a7
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-07-03 19:17:11 -0600

    Servir archivos por controlador y carpeta public

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) { }
+  constructor(private readonly filesService: FilesService, 
+    private readonly configService: ConfigService) { }
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string ) {
+
+    const path = this.filesService.getStaticProductImage(imageName);
+    // al agregar el decorador res, le decimos a nest que nosotros tomaremos el control de la respuesta de forma manual
+    // por lo que la funcionalidad de nest deja de funcionar
+    // se saltan ciertos pasos del ciclo de vida de nest, como interceptores globales
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -23,9 +39,10 @@ export class FilesController {
       throw new BadRequestException(`Make sure that the file is an image`);
     }
 
-    console.log(file);
+    // const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
-    return { fileName: file.originalname };
+    return { secureUrl };
   }
 
 }
