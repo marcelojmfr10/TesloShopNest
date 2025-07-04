989c5c0af0c5640802ba9593a56e6790c94e0d51
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-07-03 20:17:28 -0600

    Creación de usuario

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+
+    try {
+
+      const {password, ...userData} = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+      await this.userRepository.save(user);
+
+      const { password: _, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs');
+  }
+
+}
